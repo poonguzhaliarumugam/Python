82de7183db6970bd74f69a4184051489f34cf7f2
Loknath Baskar <Loknathbaskar@outlook.comm>
2022-02-06 11:49:59 -0600

    Added comments

--- PrintEven.py
+++ PrintEven.py
@@ -1,6 +1,10 @@
 #print all the even nos in range 10
+
+#logic with list comprehension
 evenlist=[x for x in range(10) if x%2==0]
 print(evenlist)
+
+#Logic with for and if condition
 evenlist =[]
 for x in range(10):
  if x%2==0:

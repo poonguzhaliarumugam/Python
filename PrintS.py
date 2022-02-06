9677da270b6e9208740af8e099a3edc5c1c555aa
Loknath Baskar <Loknathbaskar@outlook.comm>
2022-02-06 11:40:15 -0600

    modified logic

--- PrintS.py
+++ PrintS.py
@@ -1,7 +1,11 @@
 #print only the words starting with s 
 st='Print only the word that start with s in this sentence'
+#for txt in st.split():
+ #for val in txt:
+  #if val[0] == 's':
+   #print(txt)
+   #continue
+
 for txt in st.split():
- for val in txt:
-  if val[0] == 's':
-   print(txt)
-   continue
+    if txt[0] == 's':
+        print(txt)

6e59b3717e20846d8b2b31b2829d34b42c263039
Loknath Baskar <Loknathbaskar@outlook.comm>
2022-02-06 21:41:52 -0600

    Create list with first letters in word using list comprehension

---
+++ WordList.py
@@ -0,0 +1,4 @@
+#To create a list of the first letters of every word in the string below
+st='create a list of the first letters of every word in this string';
+mylist=[wrd[0] for wrd in st.split()];
+print(mylist);

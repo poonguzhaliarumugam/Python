a21da873cd2059308e35b605829d6772275aa5aa
Loknath Baskar <Loknathbaskar@outlook.comm>
2022-02-17 22:38:27 -0600

    added changes

--- master_yoda.py
+++ master_yoda.py
@@ -1,3 +1,4 @@
+#method1
 def master_yoda(text):
   i=0
   templist=[]
@@ -10,6 +11,14 @@ def master_yoda(text):
   return " ".join(templist)
 
 print (master_yoda('We are ready'))
+
+#Method2
+def master_yoda2(text):
+   wordlist=text.split()
+   wordlist=wordlist[::-1]
+   return " ".join(wordlist)
+
+print(master_yoda2("We are ready"))   
 
 
        

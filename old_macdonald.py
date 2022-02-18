7a21965f965336573f92b313bb0924582231ad34
Loknath Baskar <Loknathbaskar@outlook.comm>
2022-02-17 23:03:25 -0600

    added new changes

--- old_macdonald.py
+++ old_macdonald.py
@@ -1,3 +1,7 @@
+#Method1
+from unicodedata import name
+
+
 def old_macdonald(name):
   i=0
   namelist=[]
@@ -8,5 +12,22 @@ def old_macdonald(name):
        namelist.append(x)
     i+=1   
   return ''.join(namelist)  
-
 print(old_macdonald('macdonald'))
+
+#Method2
+def old_macdonald2(name):
+  first_half=name[0:3]
+  second_half=name[3:]
+  name=first_half.capitalize()+second_half.capitalize()
+  return name
+print(old_macdonald2('macdonald'))  
+
+#Method3
+def old_macdonald3(name):
+  first_letter=name[0]
+  second_half=name[1:3]
+  third_half=name[3:4]
+  fourth_half=name[4:]
+  name=first_letter.upper()+second_half+third_half.upper()+fourth_half
+  return name
+print(old_macdonald3("macdonald"))
